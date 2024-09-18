e8ae5621de8c09c3d98160c9485942f615c42ff0
kavikrishnaa-kn <knkavikrishnaa19@gmail.com>
2024-09-18 11:59:14 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Nav from "./components/NavBar";
+
+const App = () =>{
+  return(
+    <>
+    <div className="h-screen w-screen bg-[#6A9C89]">
+    <Nav></Nav>
+    </div>
+    </>
+  )
+}
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+
+
+const Nav = () => {
+    return(
+        <>
+    <div className="h-[8vh] w-screen flex flex-row bg-[#16423C] rounded-md gap-[50%]">
+        <div className="w-1/2 h-auto bg-[ #73605B] flex items-center justify-center ">
+        <div className="w-auto h-auto flex items-center justify-center font-bold text-gray-50 text-[150%]">
+                KAVI KRISHNAA K N
+            </div>
+        </div>
+        <div className="w-1/2 h-auto flex items-center gap-6">
+            <button type="button" className= "h-[80%] w-[6rem] bg-[#C4DAD2] rounded-[2rem]">Profile</button>
+            <button type="button" className="h-[80%] w-[6rem] bg-[#C4DAD2] rounded-[2rem]">Projects</button>
+            <button type="button" className="h-[80%] w-[6rem] bg-[#C4DAD2] rounded-[2rem]">Contact</button>
+        </div>
+    </div>
+    </>
+    )
+}
+
+export default Nav;
